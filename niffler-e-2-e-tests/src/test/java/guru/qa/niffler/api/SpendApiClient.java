f03da6e73b197f41ec576e699cdc06085bbbe146
oa <a@a.com>
2025-04-22 01:27:48 +0300

    Category tests added

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApiClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApiClient.java
@@ -1,29 +1,134 @@
 package guru.qa.niffler.api;
 
+import com.codeborne.selenide.ex.Strings;
 import guru.qa.niffler.config.Config;
+import guru.qa.niffler.model.CategoryJson;
+import guru.qa.niffler.model.CurrencyValues;
+import guru.qa.niffler.model.DataFilterValues;
 import guru.qa.niffler.model.SpendJson;
-import lombok.SneakyThrows;
 import okhttp3.OkHttpClient;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SpendApiClient {
 
-  private static final Config CFG = Config.getInstance();
+    private static final Config CFG = Config.getInstance();
 
-  private final OkHttpClient client = new OkHttpClient.Builder().build();
-  private final Retrofit retrofit = new Retrofit.Builder()
-      .baseUrl(CFG.spendUrl())
-      .client(client)
-      .addConverterFactory(JacksonConverterFactory.create())
-      .build();
+    private final OkHttpClient client = new OkHttpClient.Builder().build();
+    private final Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(CFG.spendUrl())
+            .client(client)
+            .addConverterFactory(JacksonConverterFactory.create())
+            .build();
 
-  private final SpendApi spendApi = retrofit.create(SpendApi.class);
+    private final SpendApi spendApi = retrofit.create(SpendApi.class);
 
-  @SneakyThrows
-  public SpendJson addSpend(SpendJson spend) {
-    return spendApi.addSpend(spend)
-        .execute()
-        .body();
-  }
+    public SpendJson addSpend(SpendJson spend) {
+        retrofit2.Response<SpendJson> response;
+        try {
+            response = spendApi
+                    .addSpend(spend)
+                    .execute();
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+        assertEquals(201, response.code());
+        return response.body();
+    }
+
+    public SpendJson editSpend(SpendJson spend) {
+        retrofit2.Response<SpendJson> response;
+        try {
+            response = spendApi
+                    .editSpend(spend)
+                    .execute();
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+        assertEquals(200, response.code());
+        return response.body();
+    }
+
+    public SpendJson getSpend(String id) {
+        retrofit2.Response<SpendJson> response;
+        try {
+            response = spendApi
+                    .getSpend(id)
+                    .execute();
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+        assertEquals(200, response.code());
+        return response.body();
+    }
+
+    public List<SpendJson> getAllSpends(DataFilterValues period, CurrencyValues cur) {
+        retrofit2.Response<List<SpendJson>> response;
+        try {
+            response = spendApi
+                    .getAllSpends(period, cur)
+                    .execute();
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+        assertEquals(200, response.code());
+        return response.body();
+    }
+
+    public void removeSpend(List<Strings> ids) {
+        Response<Void> response;
+        try {
+            response = spendApi
+                    .removeSpend(ids)
+                    .execute();
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+        assertEquals(200, response.code());
+    }
+
+    public CategoryJson addCategory(CategoryJson category) {
+        Response<CategoryJson> response;
+        try {
+            response = spendApi
+                    .addCategory(category)
+                    .execute();
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+        assertEquals(200, response.code());
+        return response.body();
+    }
+
+    public CategoryJson updateCategory(CategoryJson category) {
+        Response<CategoryJson> response;
+        try {
+            response = spendApi
+                    .updateCategory(category)
+                    .execute();
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+        assertEquals(200, response.code());
+        return response.body();
+    }
+
+    public List<CategoryJson> getAllCategories(boolean excludeArchived) {
+        Response<List<CategoryJson>> response;
+        try {
+            response = spendApi
+                    .getAllCategories(excludeArchived)
+                    .execute();
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+        assertEquals(200, response.code());
+        return response.body();
+    }
 }
---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/CategoryExtension.java
@@ -0,0 +1,51 @@
+package guru.qa.niffler.jupiter;
+
+import guru.qa.niffler.api.SpendApiClient;
+import guru.qa.niffler.model.CategoryJson;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.extension.*;
+import org.junit.platform.commons.support.AnnotationSupport;
+
+public class CategoryExtension implements BeforeEachCallback, ParameterResolver {
+
+    public static final ExtensionContext.Namespace NAMESPACE = ExtensionContext.Namespace.create(CreateSpendingExtension.class);
+    private final SpendApiClient spendApiClient = new SpendApiClient();
+
+    @Override
+    public void beforeEach(ExtensionContext context) {
+        AnnotationSupport.findAnnotation(context.getRequiredTestMethod(), Category.class)
+                .ifPresent(anno -> {
+
+                    CategoryJson categoryJson = new CategoryJson(
+                            null,
+                            RandomStringUtils.randomAlphanumeric(6),
+                            anno.username(),
+                            anno.archived()
+                    );
+
+                    CategoryJson created = spendApiClient.addCategory(categoryJson);
+
+                    if (anno.archived()) {
+                        CategoryJson archivedCategory = new CategoryJson(
+                                created.id(),
+                                created.name(),
+                                created.username(),
+                                true
+                        );
+
+                        created = spendApiClient.updateCategory(archivedCategory);
+                    }
+                    context.getStore(NAMESPACE).put(context.getUniqueId(), created);
+                });
+    }
+
+    @Override
+    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return parameterContext.getParameter().getType().isAssignableFrom(CategoryJson.class);
+    }
+
+    @Override
+    public CategoryJson resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return extensionContext.getStore(NAMESPACE).get(extensionContext.getUniqueId(), CategoryJson.class);
+    }
+}
